ad20fb00608636bd2d635afd78830b8cd005db3e
AakarshanSingh <95141279+AakarshanSingh@users.noreply.github.com>
2022-10-03 04:56:59 +0530

    Products,Newletter,Footer

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://i.ibb.co/cXFnLLV/3.png",
-    title: "SUMMER SALE",
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    img: "https://i.ibb.co/DG69bQ4/2.png",
-    title: "AUTUMN COLLECTION",
+    img: "https://www.kindpng.com/picc/m/37-375157_dress-png-images-free-download-girl-in-dress.png",
+    title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-    bg: "fcf1ed",
+    bg: "f7f7f7",
   },
   {
     id: 3,
@@ -47,7 +47,7 @@ export const popularProducts = [
   },
   {
     id: 2,
-    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    img: "https://www.pngall.com/wp-content/uploads/2/White-Sneakers-PNG-Clipart.png",
   },
   {
     id: 3,
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://www.pngall.com/wp-content/uploads/4/Leather-Jacket-PNG-Pic.png",
   },
   {
     id: 8,
