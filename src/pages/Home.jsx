ebaf6a05a53b1842a894f9069e0e01fd9a5e3af3
AakarshanSingh <95141279+AakarshanSingh@users.noreply.github.com>
2022-10-02 18:19:06 +0530

    Categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Announcements from "../components/Announcements";
+import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Announcements />
+      <Navbar />
+      <Slider />
+      <Categories />
+    </div>
+  );
 };
 
 export default Home;
